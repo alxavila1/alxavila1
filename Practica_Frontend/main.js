ec2b16a072b3792556892bfad202eb2d3151f051
alxavila1 <alxavila12@gmail.com>
2024-08-21 12:03:55 -0300

    update

--- Practica_Frontend/main.js
+++ Practica_Frontend/main.js
@@ -1677,3 +1677,28 @@ hacerAlgoAsincrono()
   .catch((error) => {
     console.error(error);
   });
+
+  // FUNCIONES ASINCRONAS
+
+  function obtenerUsuario() {
+    return new Promise((resolver) => {
+      setTimeout(() => {
+        resolver({ nombre: 'Carlos Monge', edad: 40 });
+      }, 1000);
+    });
+  }
+  
+  async function obtenerInformacionUsuario() {
+    try {
+      const usuario = await obtenerUsuario();
+      console.log(`Nombre: ${usuario.nombre}, Age: ${usuario.edad}`);
+    } catch (error) {
+      console.error('Hubo un error:', error);
+    }
+  }
+  
+  obtenerInformacionUsuario();
+
+  //EVENTOS:
+
+  
